b854dc35ca7a303fc4d6331c689049351fb6400b
Huy Tran <huytranle13@gmail.com>
2018-03-10 21:10:06 +0700

    Basic UI Template

---
+++ CoffeeHome/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CoffeeHome
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.MainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+            this.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void btnResize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.MainWindow.WindowState == WindowState.Normal)
+                this.MainWindow.WindowState = WindowState.Maximized;
+            else
+                this.MainWindow.WindowState = WindowState.Normal;
+        }
+
+        private void Application_Activated(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() => this.MainWindow.WindowStyle = WindowStyle.None));
+        }
+
+        private void Application_Deactivated(object sender, EventArgs e)
+        {
+            if(this.MainWindow.WindowState == WindowState.Normal || this.MainWindow.WindowState == WindowState.Maximized)
+            {
+                this.MainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+            }
+        }
+    }
+}
---
+++ CoffeeHome/HomeWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoffeeHome
+{
+    /// <summary>
+    /// Interaction logic for HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+        }
+        private void CloseCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
+    }
+}
