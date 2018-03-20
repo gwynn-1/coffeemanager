33b75226ba3acddfa99b5514cea4119a72971b47
Tai Nguyen <tainguyen2912@gmail.com>
2018-03-20 21:18:22 +0700

    Set up MVVM

---
+++ CoffeeHome/ViewModel/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeHome.TemplateView;
+using System.Collections.ObjectModel;
+
+namespace CoffeeHome.ViewModel
+{
+    class HomeViewModel : BaseViewModel
+    {
+        //private ItemField[] Items { get; }
+        public ObservableCollection<ItemField> Items { get; }
+        public HomeViewModel()
+        {
+            this.Items = new ObservableCollection<ItemField>(){
+                  new ItemField("Thức uống & Đồ ăn",new DrinkAndFoodTemplate(),"Images/drink-food.png"),
+                  new ItemField("Loại thức uống",new DrinkTypeTemplate(),"Images/drink-type.png")
+            };
+        }
+    }
+}
---
+++ CoffeeHome/ViewModel/ItemField.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CoffeeHome.ViewModel
+{
+    class ItemField: INotifyPropertyChanged
+    {
+        private string _name;
+        private object _content;
+        private ScrollBarVisibility _horizontalScrollBarVisibilityRequirement;
+        private ScrollBarVisibility _verticalScrollBarVisibilityRequirement;
+        private Thickness _marginRequirement = new Thickness(16);
+        private string menuImage;
+
+        public ItemField(string name, object content,string menuimage)
+        {
+            _name = name;
+            Content = content;
+            MenuImage = menuimage;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { this.MutateVerbose(ref _name, value, RaisePropertyChanged()); }
+        }
+
+        public object Content
+        {
+            get { return _content; }
+            set { this.MutateVerbose(ref _content, value, RaisePropertyChanged()); }
+        }
+
+        public ScrollBarVisibility HorizontalScrollBarVisibilityRequirement
+        {
+            get { return _horizontalScrollBarVisibilityRequirement; }
+            set { this.MutateVerbose(ref _horizontalScrollBarVisibilityRequirement, value, RaisePropertyChanged()); }
+        }
+
+        public ScrollBarVisibility VerticalScrollBarVisibilityRequirement
+        {
+            get { return _verticalScrollBarVisibilityRequirement; }
+            set { this.MutateVerbose(ref _verticalScrollBarVisibilityRequirement, value, RaisePropertyChanged()); }
+        }
+
+        public Thickness MarginRequirement
+        {
+            get { return _marginRequirement; }
+            set { this.MutateVerbose(ref _marginRequirement, value, RaisePropertyChanged()); }
+        }
+
+        public string MenuImage { get { return menuImage; } set { this.MutateVerbose(ref menuImage, value, RaisePropertyChanged()); } }
+
+        private void MutateVerbose<ItemField>(ref ItemField control, ItemField value, Action<PropertyChangedEventArgs> action,[CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<ItemField>.Default.Equals(control, value)) return;
+            control = value;
+            action?.Invoke(new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Action<PropertyChangedEventArgs> RaisePropertyChanged()
+        {
+            return args => PropertyChanged?.Invoke(this, args);
+        }
+    }
+}
